70b0ca2f7b45586c7681df4b5ddded722b4d7f82
NishiTakeru <takeru.west@gmail.com>
2015-11-30 22:36:29 +0900

    create new projects.

---
+++ So5rin/app/src/main/java/jp/co/isd18/so5rin/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.co.isd18.so5rin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
